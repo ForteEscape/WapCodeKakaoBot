ab70cd885a3eced868829edb1a950cdc782ffa13
KTJ <justkukaro@naver.com>
2017-11-25 15:27:42 +0900

    message response

--- WapCodeKakaoBot/routes/message.js
+++ WapCodeKakaoBot/routes/message.js
@@ -3,8 +3,12 @@ var router = express.Router();
 
 router.post('/', function(req, res, next) {
     //res.render('index', { title: 'Express' });
-    console.log(req.body);
-    res.send({'test':'ktj'});
+    console.log(req.body.content);
+    res.send({
+        "message": {
+            "text": req.body.content + ':by.ktj'
+        }
+    });
 });
 
 module.exports = router;
