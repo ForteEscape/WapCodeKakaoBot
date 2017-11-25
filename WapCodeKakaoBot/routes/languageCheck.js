f6593ae6fa9a9c73983a7d3e40a3266bddf0b227
KTJ <justkukaro@naver.com>
2017-11-25 16:54:24 +0900

    test2

--- WapCodeKakaoBot/routes/languageCheck.js
+++ WapCodeKakaoBot/routes/languageCheck.js
@@ -1,10 +1,7 @@
 function langCheck(code) {
     var count = 0;
     var extend = code.split('.')[1];
-    if(extend==null){
-        return null;
-    }
-    console.log('asdf');
+    console.log('**********');
     console.log(extend);
     return extend;
 }
