94de23744c84452125eb4b5d68035a1195c532a2
KTJ <justkukaro@naver.com>
2017-11-25 21:06:25 +0900

    show user_key and title

--- WapCodeKakaoBot/routes/get_file.js
+++ WapCodeKakaoBot/routes/get_file.js
@@ -10,6 +10,8 @@ router.get('/:user_key/:title', function (req, res, next) {
     var user_key = req.params.user_key;
     var title = req.params.title;
     var buffer = fs.readFileSync(user_key + '/' + title, 'utf8');
+    console.log(user_key)
+    console.log(title)
     console.log(buffer)
     res.send('respond with a resource');
 });
