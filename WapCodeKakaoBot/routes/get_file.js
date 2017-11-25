8cfb03b25afeef8cfe7c7b698996e34ec85e10fd
KTJ <justkukaro@naver.com>
2017-11-25 21:54:59 +0900

    change css

--- WapCodeKakaoBot/routes/get_file.js
+++ WapCodeKakaoBot/routes/get_file.js
@@ -10,8 +10,8 @@ router.get('/:user_key/:title', function (req, res, next) {
     var user_key = req.params.user_key;
     var title = req.params.title;
     var buffer = fs.readFileSync(user_key + '/' + title, 'utf8');
-    var sc = '<script type="text/javascript" src="javascripts/syntaxhighlighter/shCore.js"></script>\n' +
-        '<script type="text/javascript" src="javascripts/syntaxhighlighter/shLegacy.js"></script>\n' +
+    var sc = '<script type="text/javascript" src="/javascripts/syntaxhighlighter/shCore.js"></script>\n' +
+        '<script type="text/javascript" src="/javascripts/syntaxhighlighter/shLegacy.js"></script>\n' +
         '<script type="text/javascript" src="/javascripts/syntaxhighlighter/shBrushBash.js"></script>\n' +
         '<script type="text/javascript" src="/javascripts/syntaxhighlighter/shBrushCpp.js"></script>\n' +
         '<script type="text/javascript" src="/javascripts/syntaxhighlighter/shBrushCSharp.js"></script>\n' +
@@ -29,8 +29,8 @@ router.get('/:user_key/:title', function (req, res, next) {
         '<script type="text/javascript" src="/javascripts/syntaxhighlighter/shBrushSql.js"></script>\n' +
         '<script type="text/javascript" src="/javascripts/syntaxhighlighter/shBrushVb.js"></script>\n' +
         '<script type="text/javascript" src="/javascripts/syntaxhighlighter/shBrushXml.js"></script>\n' +
-        '<link type="text/css" rel="stylesheet" href="/stylesheets/syntaxhighlighter/shCore.css">\n' +
-        '<link type="text/css" rel="stylesheet" href="/stylesheets/syntaxhighlighter/shThemeDefault.css">\n' +
+        '<link type="text/css" rel="stylesheet" href="../../stylesheets/syntaxhighlighter/shCore.css">\n' +
+        '<link type="text/css" rel="stylesheet" href="../../stylesheets/syntaxhighlighter/shThemeDefault.css">\n' +
         '<script type="text/javascript">\n' +
         'SyntaxHighlighter.all();\n' +
         '</script>' +
