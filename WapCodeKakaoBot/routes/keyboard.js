b5ff88cb5deb4e72bd830c0193d9b57fb1f6d95c
KTJ <justkukaro@naver.com>
2017-11-25 13:08:49 +0900

    add Keyboard home test

--- WapCodeKakaoBot/routes/keyboard.js
+++ WapCodeKakaoBot/routes/keyboard.js
@@ -4,7 +4,8 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
     //res.render('index', { title: 'Express' });
-    var test = {'hi':'hello'}
+    console.log(req.toString())
+    var test = {'Keyboard':'hello'}
     res.send(test)
 });
 
