940b994070d5ded074ea0d82f5474aaee8b9b36d
KTJ <justkukaro@naver.com>
2017-11-25 15:43:15 +0900

    add case

--- WapCodeKakaoBot/routes/message.js
+++ WapCodeKakaoBot/routes/message.js
@@ -3,10 +3,21 @@ var router = express.Router();
 
 router.post('/', function(req, res, next) {
     //res.render('index', { title: 'Express' });
-    console.log(req.body.content);
+    var ans = req.body.content;
+    var ansTxt = '';
+    console.log(ans);
+    if(ans=='C'){
+        ansTxt = 'C언어 입니다.';
+    }
+    else if(ans=='C++'){
+        ansTxt = 'C++ 입니다';
+    }
+    else{
+        ansTxt = '등록되지 않은 언어입니다';
+    }
     res.send({
         "message": {
-            "text": req.body.content + ':by.ktj'
+            "text": ansTxt
         }
     });
 });
