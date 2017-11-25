110e6bfca2a7a67bf098e676012fd79ab1854e93
KTJ <justkukaro@naver.com>
2017-11-26 01:13:46 +0900

    asdf

--- WapCodeKakaoBot/routes/get_file.js
+++ WapCodeKakaoBot/routes/get_file.js
@@ -11,7 +11,7 @@ router.get('/:user_key/:title', function (req, res, next) {
     var title = req.params.title;
     var buffer = fs.readFileSync(user_key + '/' + title, 'utf8');
     var sc = '<link rel="stylesheet" href="/stylesheets/highjs/default.css">\n' +
-        '<script src="/javascripts/highjs/highlight.js"></script>\n' +
+        '<script src="/javascripts/highjs/highlight.pack.js"></script>\n' +
         '<script>hljs.initHighlightingOnLoad();</script> ';
     //console.log(user_key);
     //console.log(title);
