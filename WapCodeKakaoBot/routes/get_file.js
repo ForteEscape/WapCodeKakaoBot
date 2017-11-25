68284718c1a59c7ab4764b0995ecc33c1012f8ed
KTJ <justkukaro@naver.com>
2017-11-26 01:27:44 +0900

    chan ge

--- WapCodeKakaoBot/routes/get_file.js
+++ WapCodeKakaoBot/routes/get_file.js
@@ -10,8 +10,8 @@ router.get('/:user_key/:title', function (req, res, next) {
     var user_key = req.params.user_key;
     var title = req.params.title;
     var buffer = fs.readFileSync(user_key + '/' + title, 'utf8');
-    var sc = '<link rel="stylesheet" href="/stylesheets/highjs/darcula.css">\n' +
-        '<script src="/javascripts/highjs/highlight.js"></script>\n' +
+    var sc = '<link rel="stylesheet" href="//cdn.jsdelivr.net/highlight.js/9.5.0/styles/default.min.css">'+
+        '<script src="//cdn.jsdelivr.net/highlight.js/9.5.0/highlight.min.js"></script>'+
         '<script>hljs.initHighlightingOnLoad();</script> ';
     //console.log(user_key);
     //console.log(title);
