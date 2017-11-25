32fe9fb0febef0021f244bf3c92313051949d02f
KTJ <justkukaro@naver.com>
2017-11-25 17:52:26 +0900

    test

--- WapCodeKakaoBot/routes/message.js
+++ WapCodeKakaoBot/routes/message.js
@@ -32,9 +32,9 @@ function getTitle(text) {
     console.log('**********');
     console.log(title);
     for(var i=0;i<title.length;i++){
-        console(i);
+        console.log(i);
         if(!('a'<=title.charAt(i) && title.charAt(i)<='z' || 'A'<=title.charAt(i) && title.charAt(i)<='Z' || title.charAt(i)=='.')){
-            console(i);
+            console.log(i);
             title = title.substr(i+1,title.length);
         }
         else{
@@ -43,9 +43,9 @@ function getTitle(text) {
     }
     console.log(title);
     for(var i=0;i<title.length;i++){
-        console(i);
+        console.log(i);
         if(!('a'<=title.charAt(i) && title.charAt(i)<='z' || 'A'<=title.charAt(i) && title.charAt(i)<='Z' || title.charAt(i)=='.')){
-            console(i);
+            console.log(i);
             title = title.substr(0,i);
             break;
         }
