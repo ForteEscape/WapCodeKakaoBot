c9ba830a431ec0fdfbc0455a7fc6ec0fbdaf3c7b
KTJ <justkukaro@naver.com>
2017-11-25 13:04:37 +0900

    chat_room change get to delete

--- WapCodeKakaoBot/routes/chat_room.js
+++ WapCodeKakaoBot/routes/chat_room.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/test', function(req, res, next) {
+router.delete('/test', function(req, res, next) {
     //res.render('index', { title: 'Express' });
     var test = {'hi':'chat_room/test'}
     res.send(test)
