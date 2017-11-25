c4e9f6e3d2ac0294d1e4bb0683fdb2186780778e
KTJ <justkukaro@naver.com>
2017-11-25 21:49:54 +0900

    edit route

--- WapCodeKakaoBot/routes/get_file.js
+++ WapCodeKakaoBot/routes/get_file.js
@@ -10,27 +10,27 @@ router.get('/:user_key/:title', function (req, res, next) {
     var user_key = req.params.user_key;
     var title = req.params.title;
     var buffer = fs.readFileSync(user_key + '/' + title, 'utf8');
-    var sc = '<script type="text/javascript" src="../public/javascripts/syntaxhighlighter/shCore.js"></script>\n' +
-        '<script type="text/javascript" src="../public/javascripts/syntaxhighlighter/shLegacy.js"></script>\n' +
-        '<script type="text/javascript" src="../public/javascripts/syntaxhighlighter/shBrushBash.js"></script>\n' +
-        '<script type="text/javascript" src="../public/javascripts/syntaxhighlighter/shBrushCpp.js"></script>\n' +
-        '<script type="text/javascript" src="../public/javascripts/syntaxhighlighter/shBrushCSharp.js"></script>\n' +
-        '<script type="text/javascript" src="../public/javascripts/syntaxhighlighter/shBrushCss.js"></script>\n' +
-        '<script type="text/javascript" src="../public/javascripts/syntaxhighlighter/shBrushDelphi.js"></script>\n' +
-        '<script type="text/javascript" src="../public/javascripts/syntaxhighlighter/shBrushDiff.js"></script>\n' +
-        '<script type="text/javascript" src="../public/javascripts/syntaxhighlighter/shBrushGroovy.js"></script>\n' +
-        '<script type="text/javascript" src="../public/javascripts/syntaxhighlighter/shBrushJava.js"></script>\n' +
-        '<script type="text/javascript" src="../public/javascripts/syntaxhighlighter/shBrushJScript.js"></script>\n' +
-        '<script type="text/javascript" src="../public/javascripts/syntaxhighlighter/shBrushPhp.js"></script>\n' +
-        '<script type="text/javascript" src="../public/javascripts/syntaxhighlighter/shBrushPlain.js"></script>\n' +
-        '<script type="text/javascript" src="../public/javascripts/syntaxhighlighter/shBrushPython.js"></script>\n' +
-        '<script type="text/javascript" src="../public/javascripts/syntaxhighlighter/shBrushRuby.js"></script>\n' +
-        '<script type="text/javascript" src="../public/javascripts/syntaxhighlighter/shBrushScala.js"></script>\n' +
-        '<script type="text/javascript" src="../public/javascripts/syntaxhighlighter/shBrushSql.js"></script>\n' +
-        '<script type="text/javascript" src="../public/javascripts/syntaxhighlighter/shBrushVb.js"></script>\n' +
-        '<script type="text/javascript" src="../public/javascripts/syntaxhighlighter/shBrushXml.js"></script>\n' +
-        '<link type="text/css" rel="stylesheet" href="../public/stylesheets/syntaxhighlighter/shCore.css">\n' +
-        '<link type="text/css" rel="stylesheet" href="../public/stylesheets/syntaxhighlighter/shThemeDefault.css">\n' +
+    var sc = '<script type="text/javascript" src="/javascripts/syntaxhighlighter/shCore.js"></script>\n' +
+        '<script type="text/javascript" src="/javascripts/syntaxhighlighter/shLegacy.js"></script>\n' +
+        '<script type="text/javascript" src="/javascripts/syntaxhighlighter/shBrushBash.js"></script>\n' +
+        '<script type="text/javascript" src="/javascripts/syntaxhighlighter/shBrushCpp.js"></script>\n' +
+        '<script type="text/javascript" src="/javascripts/syntaxhighlighter/shBrushCSharp.js"></script>\n' +
+        '<script type="text/javascript" src="/javascripts/syntaxhighlighter/shBrushCss.js"></script>\n' +
+        '<script type="text/javascript" src="/javascripts/syntaxhighlighter/shBrushDelphi.js"></script>\n' +
+        '<script type="text/javascript" src="/javascripts/syntaxhighlighter/shBrushDiff.js"></script>\n' +
+        '<script type="text/javascript" src="/javascripts/syntaxhighlighter/shBrushGroovy.js"></script>\n' +
+        '<script type="text/javascript" src="/javascripts/syntaxhighlighter/shBrushJava.js"></script>\n' +
+        '<script type="text/javascript" src="/javascripts/syntaxhighlighter/shBrushJScript.js"></script>\n' +
+        '<script type="text/javascript" src="/javascripts/syntaxhighlighter/shBrushPhp.js"></script>\n' +
+        '<script type="text/javascript" src="/javascripts/syntaxhighlighter/shBrushPlain.js"></script>\n' +
+        '<script type="text/javascript" src="/javascripts/syntaxhighlighter/shBrushPython.js"></script>\n' +
+        '<script type="text/javascript" src="/javascripts/syntaxhighlighter/shBrushRuby.js"></script>\n' +
+        '<script type="text/javascript" src="/javascripts/syntaxhighlighter/shBrushScala.js"></script>\n' +
+        '<script type="text/javascript" src="/javascripts/syntaxhighlighter/shBrushSql.js"></script>\n' +
+        '<script type="text/javascript" src="/javascripts/syntaxhighlighter/shBrushVb.js"></script>\n' +
+        '<script type="text/javascript" src="/javascripts/syntaxhighlighter/shBrushXml.js"></script>\n' +
+        '<link type="text/css" rel="stylesheet" href="/stylesheets/syntaxhighlighter/shCore.css">\n' +
+        '<link type="text/css" rel="stylesheet" href="/stylesheets/syntaxhighlighter/shThemeDefault.css">\n' +
         '<script type="text/javascript">\n' +
         'SyntaxHighlighter.all();\n' +
         '</script>' +
