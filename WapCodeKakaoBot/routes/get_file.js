c40f8d735644665fffaf9efee1252624c4e5b245
DESKTOP-B8C8MKU\sehunKim <sehun5515@pukyong.ac.kr>
2017-11-26 01:35:27 +0900

    patch

--- WapCodeKakaoBot/routes/get_file.js
+++ WapCodeKakaoBot/routes/get_file.js
@@ -10,35 +10,17 @@ router.get('/:user_key/:title', function (req, res, next) {
     var user_key = req.params.user_key;
     var title = req.params.title;
     var buffer = fs.readFileSync(user_key + '/' + title, 'utf8');
-    var sc = '<script type="text/javascript" src="/javascripts/syntaxhighlighter/shCore.js"></script>\n' +
-        '<script type="text/javascript" src="/javascripts/syntaxhighlighter/shLegacy.js"></script>\n' +
-        '<script type="text/javascript" src="/javascripts/syntaxhighlighter/shBrushBash.js"></script>\n' +
-        '<script type="text/javascript" src="/javascripts/syntaxhighlighter/shBrushCpp.js"></script>\n' +
-        '<script type="text/javascript" src="/javascripts/syntaxhighlighter/shBrushCSharp.js"></script>\n' +
-        '<script type="text/javascript" src="/javascripts/syntaxhighlighter/shBrushCss.js"></script>\n' +
-        '<script type="text/javascript" src="/javascripts/syntaxhighlighter/shBrushDelphi.js"></script>\n' +
-        '<script type="text/javascript" src="/javascripts/syntaxhighlighter/shBrushDiff.js"></script>\n' +
-        '<script type="text/javascript" src="/javascripts/syntaxhighlighter/shBrushGroovy.js"></script>\n' +
-        '<script type="text/javascript" src="/javascripts/syntaxhighlighter/shBrushJava.js"></script>\n' +
-        '<script type="text/javascript" src="/javascripts/syntaxhighlighter/shBrushJScript.js"></script>\n' +
-        '<script type="text/javascript" src="/javascripts/syntaxhighlighter/shBrushPhp.js"></script>\n' +
-        '<script type="text/javascript" src="/javascripts/syntaxhighlighter/shBrushPlain.js"></script>\n' +
-        '<script type="text/javascript" src="/javascripts/syntaxhighlighter/shBrushPython.js"></script>\n' +
-        '<script type="text/javascript" src="/javascripts/syntaxhighlighter/shBrushRuby.js"></script>\n' +
-        '<script type="text/javascript" src="/javascripts/syntaxhighlighter/shBrushScala.js"></script>\n' +
-        '<script type="text/javascript" src="/javascripts/syntaxhighlighter/shBrushSql.js"></script>\n' +
-        '<script type="text/javascript" src="/javascripts/syntaxhighlighter/shBrushVb.js"></script>\n' +
-        '<script type="text/javascript" src="/javascripts/syntaxhighlighter/shBrushXml.js"></script>\n' +
-        '<link type="text/css" rel="stylesheet" href="../../stylesheets/syntaxhighlighter/shCore.css">\n' +
-        '<link type="text/css" rel="stylesheet" href="../../stylesheets/syntaxhighlighter/shThemeDefault.css">\n' +
-        '<script type="text/javascript">\n' +
-        'SyntaxHighlighter.all();\n' +
-        '</script>' +
-        '<body Onload="dp.SyntaxHighlighter.HighlightAll(\'code\');">';
-    console.log(user_key);
-    console.log(title);
-    console.log(buffer);
-    res.send(sc+'<pre name="code" class="brush:cpp;">'+buffer+'</pre>');
+    var sc = '<link rel="stylesheet" href="//cdn.jsdelivr.net/highlight.js/9.5.0/styles/default.min.css">'+
+        '<script src="//cdn.jsdelivr.net/highlight.js/9.5.0/highlight.min.js"></script>'+
+        '<script>hljs.initHighlightingOnLoad();</script> ';
+    //console.log(user_key);
+    //console.log(title);
+    //console.log(buffer);
+    buffer = buffer.replace('<', '&lt');
+    buffer = buffer.replace('>', '&gt');
+    var st = sc+'<pre><code>' + buffer + '</code></pre>';
+    console.log(st);
+    res.send(st);
 });
 
 module.exports = router;
